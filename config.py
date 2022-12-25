b7a42e23abcee869453535f739f689cb40c96256
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2022-12-25 13:17:32 +0530

    config vars

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
-import os
+# Copyright ©️ 2022 Sanila Ranatunga. All Rights Reserved
+# You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
+# ##Credits - [YouTube-Bot](https://github.com/sanila2007/YouTube_bot_telegram)
 
+# Read GNU General Public License v3.0: https://github.com/sanila2007/YouTube_bot_telegram/blob/mai/LICENSE
+# Don't forget to follow github.com/sanila2007 because I am doing these things for free and open source
+# Star, fork, enjoy!
+
+import os
 
 class Config(object):
     API_ID = int(os.environ.get("API_ID", 12345678))
