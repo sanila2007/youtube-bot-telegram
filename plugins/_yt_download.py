ba0d5170e40506f390184c615cf2cab4ee67a592
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2023-03-12 17:29:14 +0530

    Update _yt_download.py

--- plugins/_yt_download.py
+++ plugins/_yt_download.py
@@ -1,3 +1,5 @@
+# Copyright ©️ 2023 Sanila Ranatunga. All Rights Reserved
+
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
